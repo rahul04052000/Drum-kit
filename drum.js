d41c8b8347d34cb56b3ab5cf3e16e49bfd6da4ab
rahul04052000 <90526719+rahul04052000@users.noreply.github.com>
2022-04-17 22:47:47 +0530

    Add files via upload

---
+++ drum.js
@@ -0,0 +1,54 @@
+var x = document.querySelectorAll(".drum").length;
+for (var i = 0; i < x; i++) {
+
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var button = this.innerHTML;
+        makesound(button);
+
+    });
+}
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+})
+
+function makesound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("crash.mp3");
+            kick.play();
+            break;
+
+
+
+
+        default: console.log(button);
+            break;
+    }
+}
+
+
